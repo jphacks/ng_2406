a678fa80322505f7d2fa32ed6102a4da68ec3d09
waka320 <yukiwakamatsu6a@gmail.com>
2024-10-26 14:49:51 +0900

    一応動くやつ

---
+++ backend/app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+
+def create_app():
+    app = Flask(__name__)
+    
+    # CORSの設定
+    CORS(app)
+
+    # コンフィグをロード
+    app.config.from_object('config.Config')
+
+    # JWTの初期化
+    jwt = JWTManager(app)
+
+    # ルーティングを登録
+    from .routes import api
+    app.register_blueprint(api, url_prefix='/api')
+
+    return app
