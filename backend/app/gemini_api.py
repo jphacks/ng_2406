6d4181e6cec6e59cb6c1d35be6c2a5323de868da
waka320 <yukiwakamatsu6a@gmail.com>
2024-10-27 00:11:54 +0900

    エンドポイントは全部完成した

--- backend/app/gemini_api.py
+++ backend/app/gemini_api.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import sys
 from app.weather_api import WeatherAPI
 
+
 class GeminiAPI:
     def __init__(self):
         # 環境変数の読み込み
@@ -17,15 +18,25 @@ class GeminiAPI:
         self.weather = WeatherAPI()
     
     def generate_content(self, data_action):
+        '''
+        ユーザーからの情報をもとにフィードバックを作成
+        input(str) : data_action
+        output(dict) : response
+        '''
         # 天気情報の取得
         weather_data = self.get_wether_data()
         # ユーザーからの情報をもとにフィードバックを作成
         prompt = self.generate_prompt(data_action)
         actions = self.action_extract(prompt)
-        responce = self.action_response(actions, weather_data)
-        return responce
+        response = self.action_response(actions, weather_data)
+        return response
 
     def get_wether_data(self):
+        '''
+        天気情報を取得する
+        input : None
+        output : str : 天気情報
+        '''
         nagoya_city_number = 230010
         weather_data = self.weather.get_weather(nagoya_city_number)
         prompt = (weather_data + "ここから天気の情報を取り出し、20字以内のアドバイスをください")
@@ -33,6 +44,11 @@ class GeminiAPI:
         return weather_data
 
     def generate_prompt(self, data_action):
+        '''
+        ユーザーが入力した内容から行動を抜き出すためのプロンプトを作成
+        input(str) : data_action
+        output(str) : prompt
+        '''
         prompt_action = data_action
         prompt = (prompt_action
         + "という文章から行動を抜き出してpythonの配列として出力してください。")
@@ -74,16 +90,44 @@ class GeminiAPI:
         input: actions(list[str]) : ユーザーが入力した行動, weather_data(str) : 天気情報
         output(dict) : response
         '''
-        responce = {'data' : []}
-        responce['data'].append({
+        response = {'data' : []}
+        response['data'].append({
             'face': 2,
             'title': '天気情報',
-            'description': weather_data.strip()
+            'description': weather_data
         })
         for action in actions:
-            responce['data'].append({
-                'face': 1,
+            prompt_description = action + "の気をつけた方が良いポイントをおばあちゃん口調で60字以内で教えてください。"
+            description = self.model.generate_content(prompt_description).text
+            face = self._get_facescore(action)
+            response['data'].append({
+                'face': face,
                 'title': action,
-                'description': 'AIからの注意'
+                'description': description
             })
-        return responce
+        return response
+
+    def _get_facescore(self, action):
+        '''
+        行動から危険度を考える（危険度が高い順に2, 1, 0で返す）
+        input : action(str) : 行動
+        output : int : 感情の番号
+        '''
+        prompt_face = (
+            f"{action}の最中に危険が起こる一般的な確率を評価してください。"
+            "評価結果を以下の数値のどれか1つで示してください: "
+            "2は危険度が高い、1は中程度、0は低い。"
+            "回答は必ず数値のみで「0」「1」「2」のいずれかを返してください。"
+        )
+        
+        while True:
+            # プロンプトの応答取得
+            face = self.model.generate_content(prompt_face).text.strip()
+            
+            # 正規表現で数値判定
+            if re.fullmatch("[0-2]", face):
+                print(face)
+                break
+            else:
+                print(f"不正な応答 '{face}' が返されました。再試行します。")
+        return face
