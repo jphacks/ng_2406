33bf7fc7c6d83706b92bdf8c151a5419b6cf6aba
waka320 <yukiwakamatsu6a@gmail.com>
2024-11-04 14:19:51 +0900

    adjust UI#36

--- frontend/src/components/GrandmaText.js
+++ frontend/src/components/GrandmaText.js
@@ -3,7 +3,16 @@ import { Box, Typography } from '@mui/material';
 import titleImage from '../images/title.png';
 
 const GrandmaText = ({ text }) => (
-    <Box sx={{ display: 'flex', alignItems: 'center', mt: 2 }}>
+    <Box
+        sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            overflow: 'hidden',
+            mt: 2
+        }}
+    >
         <Box
             component="img"
             sx={{
@@ -15,7 +24,17 @@ const GrandmaText = ({ text }) => (
             alt="タイトルおばあ"
             src={titleImage}
         />
-        <Typography className="zen-maru-gothic-regular" variant="h4" component="h1">
+        <Typography
+            variant="h5"
+            component="div"
+            sx={{
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                maxWidth: '100%',
+                fontFamily: '"Zen Maru Gothic", sans-serif',
+            }}
+        >
             「{text}」
         </Typography>
     </Box>
